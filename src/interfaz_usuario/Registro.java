e7c478ffe8fc9381e28bc033448e0bf2928f076c
jneiracampos <j.neira@uniandes.edu.co>
2022-03-30 19:46:19 -0500

    ...

--- src/interfaz_usuario/Registro.java
+++ src/interfaz_usuario/Registro.java
@@ -1,17 +1,12 @@
 package interfaz_usuario;
 
 import java.time.LocalDate;
-
 import modelo.Participante;
 import modelo.Proyecto;
 
 public class Registro {
 	
-	private Proyecto proyecto;
 	
 	
-	public void createProyecto(String nombre, String descripcion, LocalDate fechaInicio, LocalDate fechaFin, Participante participanteInicial) {
-
-	}
 	
 }
