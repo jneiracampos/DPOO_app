80318e5e1a5cf1ba00e168493a017e00144e2f8c
jneiracampos <j.neira@uniandes.edu.co>
2022-03-30 20:48:07 -0500

    Avances

--- src/interfaz_usuario/App.java
+++ src/interfaz_usuario/App.java
@@ -1,9 +1,71 @@
 package interfaz_usuario;
 
-public class App {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class App {
+	
+	/**
+	 * Ejecuta la aplicacion: le muestra el menu al usuario y le pide que ingrese 
+	 * una opcion, y ejecuta la opcion seleccionada por el usuario. Este proceso se
+	 * repite hasta que el usuario seleccione la opcion de abandonar la aplicacion.
+	 */
+	public void ejecutaraplicacion() {
+		boolean continuar = true;
+		while(continuar) {
+			try {
+				mostrarMenu();
+				int opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opcion"));
+				if (opcion_seleccionada == 1) {
+					System.out.println("Saliendo de la aplicacion...");
+					continuar = false;
+				}
+				
+			}
+			catch (NumberFormatException e) {
+				System.out.println("Debe seleccionar uno de las opciones");
+			}
+		}
+	}
+	
+	
+	/**
+	 * Muestra al usuario el menu con las opciones para que escoja la siguiente
+	 * accion que quiere ejecutar.
+	 */
+	public void mostrarMenu() {
+		System.out.println("\nOpciones de la aplicacion\n");
+	}
+	
+	/**
+	 * Este metodo sirve para imprimir un mensaje en la consola pidiendole
+	 * informacion al usuario y luego leer lo que escribe el usuario.
+	 * 
+	 * @param mensaje
+	 * @return La cadena de caracteres que el usuario escriba como respuesta.
+	 */
+	public String input(String mensaje) {
+		try {
+			System.out.print(mensaje + ": ");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			return reader.readLine();
+		}
+		catch (IOException e) {
+			System.out.println("Error leyendo de la consola");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Este es el metodo principal de la aplicacion, con el que inicia la ejecucion 
+	 * de la aplicacion
+	 * @param args Parametros introducidos en la linea de comandos al invocar la aplicacion
+	 */
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		App consola = new App();
+		consola.ejecutaraplicacion();
 	}
 
 }
--- src/interfaz_usuario/Registro.java
+++ src/interfaz_usuario/Registro.java
@@ -1,12 +1,5 @@
 package interfaz_usuario;
 
-import java.time.LocalDate;
-import modelo.Participante;
-import modelo.Proyecto;
-
 public class Registro {
-	
-	
-	
 	
 }
