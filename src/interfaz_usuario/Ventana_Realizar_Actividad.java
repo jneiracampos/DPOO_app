25a8d480b67afedb7c65410c11e4547900c17add
valentinac2311 <v.calderonm@uniandes.edu.co>
2022-05-01 11:19:42 -0500

    realizar actividad

--- src/interfaz_usuario/Ventana_Realizar_Actividad.java
+++ src/interfaz_usuario/Ventana_Realizar_Actividad.java
@@ -1,89 +1,129 @@
-package interfaz_usuario;
+	package interfaz_usuario;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+	import java.awt.*;
+	import java.awt.event.*;
+	import java.time.LocalDate;
+	import java.time.LocalTime;
+	import java.time.ZoneId;
+	import javax.swing.*;
+	import javax.swing.GroupLayout.*;
+	import com.github.lgooddatepicker.components.TimePicker;
+	import com.toedter.calendar.JDateChooser;
+	import modelo.Actividad;
+	import modelo.Participante;
 
-@SuppressWarnings("serial")
-public class Ventana_Realizar_Actividad extends JFrame implements ActionListener {
-	
-	private JPanel panelCentro;
-	private JPanel panelNorte;
-	private JPanel panelW;
-	private JPanel panelE;
-	private JPanel panelS;
-	
-	private Ventana_Inicio ventanaInicio;
-	
-	public Ventana_Realizar_Actividad(Ventana_Opciones padre) {
-		ventanaOpciones = padre;
+	@SuppressWarnings("serial")
+	public class Ventana_Realizar_Actividad extends JFrame implements ActionListener {
 		
-		panelW = new JPanel();
-		panelW.setOpaque(true);
-		add(panelW, BorderLayout.WEST);
+		private Ventana_Opciones ventanaOpciones;
+		private JPanel panelCentro;
+		private JPanel panelSur;
+		private JPanel panelNorte;
+		private JTextField txtFieldNombre;
+		private JTextField txtFieldDescripcion;
+		private JTextField txtFieldCorreoParticipante;
 		
-		panelE = new JPanel();
-		panelE.setOpaque(true);
-		add(panelE, BorderLayout.EAST);
-		
-		panelS = new JPanel();
-		panelS.setOpaque(true);
-		add(panelS, BorderLayout.SOUTH);
-		panelS.setPreferredSize(new Dimension(400,80));
-		
-		addButtons();
-		addNorthLabel();
-		
-		setSize(400, 400);
-		setTitle("Realizar Actividad");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
-	}
-	
-	private void addNorthLabel() {
-		panelNorte = new JPanel();
-		panelNorte.setOpaque(true);
-		add(panelNorte, BorderLayout.NORTH);
-		panelNorte.setPreferredSize(new Dimension(400,40));
-		
-		JLabel txt = new JLabel("La actividad va a realizar: ");
-		panelNorte.add(txt);
-	}
-	
-	private void addButtons() {
-		panelCentro = new JPanel();
-		panelCentro.setOpaque(true);
-		panelCentro.setLayout(new GridLayout(5, 1, 0, 12));
-
-		add(panelCentro, BorderLayout.CENTER);
-
-		JButton btnYo = new JButton("Yo");
-		JButton btnOtro = new JButton("Otro Usuario");
-		
-
-		panelCentro.add(btnYo);
-		panelCentro.add(btnOtro);
-
-		
-		btnYo.addActionListener(this);
-		btnOtro.addActionListener(this);
-
-	}
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		String comando = e.getActionCommand();
-		
-		if (comando.equals("Yo")) {
-			setVisible(false);
-			new Ventana_Yo(this);
+		public Ventana_Realizar_Actividad(Ventana_Opciones padre) {
+			ventanaOpciones = padre;
+			addTextField();
+			addButtons();
+			addNorthLabel();
+			setSize(500, 350);
+			setTitle("");
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+			setVisible(true);
 		}
-		else if (comando.equals("Otro Usuario")){
-			setVisible(false);
-			new Ventana_Otro_Usuario(this);
+		
+		private void addNorthLabel() {
+			panelNorte = new JPanel();
+			panelNorte.setOpaque(true);
+			add(panelNorte, BorderLayout.NORTH);
+
+			JLabel txt = new JLabel("Realizar una actividad");
+			panelNorte.add(txt);
+		}
+		
+		private void addTextField() {
+			panelCentro = new JPanel();
+			panelCentro.setOpaque(true);
+			add(panelCentro, BorderLayout.CENTER);
+			
+			JLabel txtCorreoParticipante = new JLabel("Correo del participante:");
+			JLabel txtSolicitud = new JLabel("Ingrese los siguientes datos sobre la actividad...");
+			JLabel txtNombre = new JLabel("Nombre:");
+			JLabel txtDescripcion = new JLabel("Descripcion:");
+			JLabel txtTipo = new JLabel("Tipo:");
+			JLabel txtCronometro = new JLabel("Cronometro");
+			JLabel txtNull = new JLabel();
+			JLabel txtNull1 = new JLabel();
+
+			
+			txtFieldCorreoParticipante = new JTextField();
+			txtFieldNombre = new JTextField();
+			txtFieldDescripcion = new JTextField();
+			txtFieldCronometro = new JTextField();
+			
+			
+			GroupLayout layout = new GroupLayout(panelCentro);
+			panelCentro.setLayout(layout);
+			layout.setAutoCreateGaps(true);
+			layout.setAutoCreateContainerGaps(true);
+			
+			GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+			hGroup.addGroup(layout.createParallelGroup().addComponent(txtCorreoParticipante).addComponent(txtSolicitud).addComponent(txtNombre).addComponent(txtDescripcion).addComponent(txtTipo).addComponent(txtCronometro));
+			hGroup.addGroup(layout.createParallelGroup().addComponent(txtFieldCorreoParticipante).addComponent(txtNull).addComponent(txtFieldNombre).addComponent(txtFieldDescripcion).addComponent(txtNull1).addComponent(txtFieldCronometro));
+			layout.setHorizontalGroup(hGroup);
+			
+			GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+			vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(txtCorreoParticipante).addComponent(txtFieldCorreoParticipante));
+			vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(txtSolicitud).addComponent(txtNull));
+			vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(txtNombre).addComponent(txtFieldNombre));
+			vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(txtDescripcion).addComponent(txtFieldDescripcion));
+			vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(txtTipo).addComponent(txtNull1));
+			vGroup.addGroup(layout.createParallelGroup(Alignment.BASELINE).addComponent(txtCronometro).addComponent(txtFieldCronometro));
+			layout.setVerticalGroup(vGroup);
+		}
+		
+		private void addButtons() {
+			panelSur = new JPanel();
+			panelSur.setOpaque(true);
+			add(panelSur, BorderLayout.SOUTH);
+			
+			JButton btnRegistrar = new JButton("Registrar");
+			JButton btnVolver = new JButton("Volver");
+			
+			panelSur.add(btnVolver);
+			panelSur.add(btnRegistrar);
+			
+			btnRegistrar.addActionListener(this);
+			btnVolver.addActionListener(this);
 		}
 
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String comando = e.getActionCommand();
+			
+			if (comando.equals("Realizar")) {
+				String correo = txtFieldCorreoParticipante.getText();
+				String nombreActividad = txtFieldNombre.getText();
+				String tipo = null;
+				Participante participante = Registro.getParticipantePorCorreo(correo);
+				
+				if (nombreActividad.equals("")) {
+					JOptionPane.showMessageDialog(this, "Recuerde ingresar el nombre de la actividad", "Aviso",
+					JOptionPane.INFORMATION_MESSAGE);
+				}
+				else if (descripcionActividad.equals("")) {
+					JOptionPane.showMessageDialog(this, "Recuerde ingresar la descripcion de la actividad", "Aviso",
+					JOptionPane.INFORMATION_MESSAGE);
+				
+					}
+				}
+			}
+			else if (comando.equals("Volver")) {
+				setVisible(false);
+				ventanaOpciones.setVisible(true);
+			}
+		}
 	}
-
-}
